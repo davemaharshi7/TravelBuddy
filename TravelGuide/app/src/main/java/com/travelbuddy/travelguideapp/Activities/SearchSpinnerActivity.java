6784b0d96534b70d89238ede42bf81a9940533c8
davemaharshi7 <39943075+davemaharshi7@users.noreply.github.com>
2019-03-22 19:32:58 +0530

    Search City Activity Fixed
    Submit button on search city activity disabled while data is loading

--- TravelGuide/app/src/main/java/com/travelbuddy/travelguideapp/Activities/SearchSpinnerActivity.java
+++ TravelGuide/app/src/main/java/com/travelbuddy/travelguideapp/Activities/SearchSpinnerActivity.java
@@ -16,6 +16,7 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -35,6 +36,7 @@ public class SearchSpinnerActivity extends BaseActivity {
     Hashtable<String,String> map;
     Button submit;
     Spinner searchSpinner;
+    TextView loading;
     SharedPreferences shared;
 
     @Override
@@ -59,6 +61,8 @@ public class SearchSpinnerActivity extends BaseActivity {
                             ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(SearchSpinnerActivity.this, android.R.layout.simple_spinner_item, city);
                             areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                             searchSpinner.setAdapter(areasAdapter);
+                            submit.setEnabled(true);
+                            loading.setVisibility(View.INVISIBLE);
 
                         } else {
                             Log.d("EEEE", "Error getting documents: ", task.getException());
@@ -85,21 +89,21 @@ public class SearchSpinnerActivity extends BaseActivity {
         rb.setBackgroundColor(getResources().getColor(R.color.colorAccent));
         rb.setTextColor(getResources().getColor(R.color.white));
 
-
+        loading = findViewById(R.id.loadingTextBox);
         searchSpinner = (Spinner) findViewById(R.id.searchSpinner);
         submit = (Button) findViewById(R.id.submit_search);
+        submit.setEnabled(false);
         map = new Hashtable<>();
         //spinner.setOnItemClickListener();
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String cityName = searchSpinner.getSelectedItem().toString();
-                if(TextUtils.isEmpty(cityName))
-                {
-                    printMessage("Please Wait for cities to load.....");
+               // String cityName = searchSpinner.getSelectedItem().toString();
+                if (searchSpinner.getSelectedItem().toString().trim().equals("")) {
+                    printMessage("Wait for Data to load!!This may be due low internet connection");
                 }
                 else {
-                    String cityID = map.get(cityName);
+                    String cityID = map.get(searchSpinner.getSelectedItem().toString());
                     SharedPreferences.Editor editor = shared.edit();
                     editor.putString("city_id",cityID);
                     editor.commit();
